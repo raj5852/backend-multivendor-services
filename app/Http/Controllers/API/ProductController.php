459db4d6b33f59629ba77acde5c6b0808a937f8c
raj <rajkumer2001@gmail.com>
2023-05-23 16:21:49 +0600

    arr

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -55,7 +55,7 @@ class ProductController extends Controller
         }else{
             return response()->json([
                 'status'=>400,
-                'message'=>'Not found'
+                'message'=>[]
             ]);
         }
     }
