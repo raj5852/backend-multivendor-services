bd333dc12f4113b913758c5bb2b4f22c6f86ef7d
raj <rajkumer2001@gmail.com>
2023-06-03 18:57:04 +0600

    comission

--- app/Service/Affi/DashboardService.php
+++ app/Service/Affi/DashboardService.php
@@ -49,6 +49,57 @@ class DashboardService
     }
     static  function orderVsRevenue()
     {
+        $today_data = Order::selectRaw('HOUR(created_at) AS hour, COUNT(*) AS order_count, SUM(afi_amount) AS comission')
+            ->whereDate('created_at', Carbon::today())
+            ->where('affiliator_id',auth()->user()->id)
+            ->whereIn('status', [Status::Pending->value, Status::Progress->value, Status::Delivered->value])
+            ->groupBy('hour')
+            ->orderBy('hour', 'asc')
+            ->get();
 
+        $daily_data = [
+            'label' => $today_data->pluck('hour')->toArray(),
+            'order' => $today_data->pluck('order_count')->toArray(),
+            'comission' => $today_data->pluck('comission')->toArray(),
+        ];
+
+
+
+        $weeklyData = Order::selectRaw('DATE(created_at) AS date, COUNT(*) AS order_count, SUM(afi_amount) AS comission')
+        ->whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek(), Carbon::now()->subWeek()->endOfWeek()])
+        ->where('affiliator_id',auth()->user()->id)
+            ->whereIn('status', [Status::Pending->value, Status::Progress->value, Status::Delivered->value])
+            ->groupBy('date')
+            ->orderBy('date', 'asc')
+            ->get();
+
+        $weekly_data = [
+            'label' => $weeklyData->pluck('date')->toArray(),
+            'order' => $weeklyData->pluck('order_count')->toArray(),
+            'comission' => $weeklyData->pluck('comission')->toArray(),
+        ];
+
+
+
+        $monthlyData = Order::selectRaw('DATE(created_at) AS date, COUNT(*) AS order_count, SUM(afi_amount) AS comission')
+        ->whereBetween('created_at', [Carbon::now()->subMonth()->startOfMonth(), Carbon::now()->subMonth()->endOfMonth()])
+        ->where('affiliator_id',auth()->user()->id)
+            ->whereIn('status', [Status::Pending->value, Status::Progress->value, Status::Delivered->value])
+            ->groupBy('date')
+            ->orderBy('date', 'asc')
+            ->get();
+
+        $monthly_data = [
+            'label' => $monthlyData->pluck('date')->toArray(),
+            'order' => $monthlyData->pluck('order_count')->toArray(),
+            'comission' => $monthlyData->pluck('comission')->toArray(),
+        ];
+
+
+        return response()->json([
+            'daily' => $daily_data,
+            'weekly' => $weekly_data,
+            'monthly' => $monthly_data
+        ]);
     }
 }
