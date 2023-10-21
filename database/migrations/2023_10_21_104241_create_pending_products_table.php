72fd259e090d4adaa5c5cc49688340f2933918e3
raj5852 <rajkumer2001@gmail.com>
2023-10-21 13:35:56 +0600

    ok

--- database/migrations/2023_10_21_104241_create_pending_products_table.php
+++ database/migrations/2023_10_21_104241_create_pending_products_table.php
@@ -16,12 +16,12 @@ return new class extends Migration
         Schema::create('pending_products', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('product_id');
-            $table->text('short_description');
-            $table->longText('long_description');
-            $table->longText('specification');
-            $table->longText('specification_ans');
-            $table->string('image');
-            $table->longText('images');
+            $table->text('short_description')->nullable();
+            $table->longText('long_description')->nullable();
+            $table->longText('specification')->nullable();
+            $table->longText('specification_ans')->nullable();
+            $table->string('image')->nullable();
+            $table->longText('images')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
