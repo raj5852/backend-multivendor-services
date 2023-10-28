bc948feab6fb09d3eea8514d3b4ef73eb5286ebe
raj5852 <rajkumer2001@gmail.com>
2023-10-28 12:13:30 +0600

    ok

--- app/Http/Controllers/API/Vendor/ProductManageController.php
+++ app/Http/Controllers/API/Vendor/ProductManageController.php
@@ -343,6 +343,7 @@ class ProductManageController extends Controller
                 })->toArray();
 
                 info($specificationdata);
+                info(1);
 
                 if (($product->short_description != request('short_description')) || ($product->long_description != request('long_description'))  || request()->hasFile('image') || request()->hasFile('images') || $product->specifications != $specificationdata) {
                     $pendingproductdetails =  PendingProduct::where('product_id', $product->id)->first();
