72cf794b85cb0311d36d64bc03fd9bade8e920d7
raj5852 <rajkumer2001@gmail.com>
2023-11-08 15:46:37 +0600

    ok

--- app/Services/ShowAllService.php
+++ app/Services/ShowAllService.php
@@ -17,7 +17,9 @@ class ShowAllService
             ->latest()
             ->withAvg('servicerating','rating')
             ->with(['user:id,name,image','firstpackage:id,price,vendor_service_id'])
+            ->select('id','title','servicerating_avg_rating','user','firstpackage')
             ->paginate(12);
+
         foreach($datas as $data){
             if($data->servicerating_avg_rating == null){
                 $data->servicerating_avg_rating = '0.00';
