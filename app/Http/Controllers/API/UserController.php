fe363e9b5b7ff52ba8733a10842d0dfcf31340be
raj5852 <rajkumer2001@gmail.com>
2023-11-11 10:10:42 +0600

    search

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -291,7 +291,7 @@ class UserController extends Controller
             ->when(request('name') == 'active', function ($q) {
                 return $q->where('status', 'active');
             })
-            ->when($request->email, fn ($q, $email) => $q->where('email', 'like', "%{$email}%"))
+            ->when($request->email != '', fn ($q, $email) => $q->search($email))
             ->latest()
             ->paginate(10)
             ->withQueryString();
