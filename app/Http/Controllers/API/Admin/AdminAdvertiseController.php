e12b7e5e4e8f0d588976965e35d1e3c22db2587e
raj5852 <rajkumer2001@gmail.com>
2023-12-24 13:49:56 +0600

    user data

--- app/Http/Controllers/API/Admin/AdminAdvertiseController.php
+++ app/Http/Controllers/API/Admin/AdminAdvertiseController.php
@@ -33,7 +33,7 @@ class AdminAdvertiseController extends Controller
             ->latest()
             ->when(request('search'), fn ($q, $search) => $q->where('unique_id', 'like', "%{$search}%"))
             ->where('is_paid', 1)
-            ->select('id', 'campaign_name', 'campaign_objective', 'budget_amount', 'start_date', 'end_date', 'is_paid', 'created_at', 'status', 'unique_id')
+            ->select('id', 'campaign_name', 'campaign_objective', 'budget_amount', 'start_date', 'end_date', 'is_paid', 'created_at', 'status', 'unique_id','user_id')
             ->with('user:id,name,email')
             ->paginate(10);
 
